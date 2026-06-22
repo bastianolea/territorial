5d25cb09af7158390f6596296a0bc42ed8bfc1a4
bastianolea <bastianolea@gmail.com>
2026-06-21 22:37:51 -0400

    idea validar_regiones()

---
+++ R/validar_regiones.R
@@ -0,0 +1,9 @@
+validar_regiones <- function(nombre_region) {
+  # regiones en mayúsculas
+  # regiones sin mayúsculas
+  # Ñuble se escribe con Ñ
+  # O'Higgins sin apóstrofo
+  # O'Higgins con apóstrofo incorrecto
+  # regiones sin tilde
+  # regiones con mayúsculas en los artículos
+}
